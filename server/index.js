409ced114f1c7fb1f923bbd957fe7ddff1039321
Steve Kim <emailstevenkim@gmail.com>
2017-05-23 01:43:07 -0700

    Update server index.js

--- server/index.js
+++ server/index.js
@@ -7,6 +7,6 @@ app.use(express.static(__dirname + '/../react-client/dist'));
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('listening on port 3000!');
 });
